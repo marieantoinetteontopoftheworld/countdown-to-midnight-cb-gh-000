60a9617ee3b3b0f755e5cad0141e6e8181e8f8fd
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-02 14:18:24 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,11 +4,23 @@ def countdown(start)
   count = start
 
   while count > 0
-    puts count
+    puts "${count} SECOND(S)!"
+
+    count -= 1
+  end
+
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(start)
+  count = start
+
+  while count > 0
+    puts "${count} SECOND(S)!"
 
     count -= 1
     sleep(1)
   end
 
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
